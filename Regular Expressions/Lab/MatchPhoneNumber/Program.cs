200ccb6146069a3895c357fc90bf705fa1e7eb28
radomirKrastev <radomirkostantinov@gmail.com>
2019-07-11 14:57:37 +0300

    Lab problem Two

---
+++ Regular Expressions/Lab/MatchPhoneNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace MatchPhoneNumber
+{
+    using System;
+    using System.Text.RegularExpressions;
+    using System.Linq;
+    public class Program
+    {
+        public static void Main()
+        {
+            var regex = @"([+]359 2 \d{3} \d{4})|([+]359-2-\d{3}-\d{4})\b";
+            var phoneNumbers = Console.ReadLine();
+
+            MatchCollection phoneMatches = Regex.Matches(phoneNumbers, regex);
+
+            var legitPhoneNumbers = phoneMatches
+                .Cast<Match>()
+                .Select(x => x.Value)
+                .ToArray();
+
+            Console.WriteLine(string.Join(", ", legitPhoneNumbers));
+        }
+    }
+}
