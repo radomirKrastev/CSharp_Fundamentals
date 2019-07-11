75f29a051587b667c402aef90fad9f1b85e45454
radomirKrastev <radomirkostantinov@gmail.com>
2019-07-11 19:28:38 +0300

    Regular expressions exercise problem one

---
+++ Exercise/Furniture/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Furniture
+{
+    using System;
+    using System.Text.RegularExpressions;
+    using System.Collections.Generic;
+    public class Program
+    {
+        public static void Main()
+        {
+            var regex = @">>(?<product>[A-Za-z]+)<<(?<price>\d+(\.\d+)?)!(?<quantity>\d+)";
+            var input = Console.ReadLine();
+            var purchases = string.Empty;
+            var totalSum = 0.0;
+            while (input != "Purchase")
+            {
+                purchases += input;
+                input = Console.ReadLine();
+            }
+
+            MatchCollection validPurchases = Regex.Matches(purchases, regex);
+
+            Console.WriteLine("Bought furniture:");
+
+            foreach (Match purchase in validPurchases)
+            {
+                var product = purchase.Groups["product"].Value;
+                var price = double.Parse(purchase.Groups["price"].Value);
+                var quantity = int.Parse(purchase.Groups["quantity"].Value);
+                totalSum += price*quantity;
+                Console.WriteLine(product);
+            }
+
+            Console.WriteLine($"Total money spend: {totalSum:F2}");
+        }
+    }
+}
