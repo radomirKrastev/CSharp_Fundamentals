a5fd93c82611bcc137336b47835e17a6fc89b798
radomirKrastev <radomirkostantinov@gmail.com>
2019-07-14 12:12:54 +0300

    Regular expressions problem 6

--- Regular Expressions/Exercise/ExtractEmails/Program.cs
+++ Regular Expressions/Exercise/ExtractEmails/Program.cs
@@ -7,10 +7,13 @@
         public static void Main()
         {
             var data = Console.ReadLine();
-            var emailPattern = @"^(?<!.-_)[a-zA-Z0-9]+[a-zA-Z0-9-_.]+[a-zA-Z0-9]+(?!.-_)[@]([a-zA-Z]+([.-][a-zA-Z]+)+)";
-
+            var emailPattern = @"(^|(?<=\s))[a-zA-Z0-9]+[a-zA-Z0-9-_.]+[a-zA-Z0-9]+(?!.-_)[@]([a-zA-Z]+([.-][a-zA-Z]+)+)";
             MatchCollection matchingEmails = Regex.Matches(data, emailPattern);
 
+            foreach (var email in matchingEmails)
+            {
+                Console.WriteLine(email);
+            }
         }
     }
 }
