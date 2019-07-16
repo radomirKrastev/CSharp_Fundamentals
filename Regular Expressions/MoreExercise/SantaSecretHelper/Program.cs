4c9aef05d81c66b844abbb16cdee02a04ef879a3
radomirKrastev <radomirkostantinov@gmail.com>
2019-07-16 11:32:49 +0300

    Regular expressions problem

---
+++ Regular Expressions/MoreExercise/SantaSecretHelper/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace SantaSecretHelper
+{
+    using System;
+    using System.Text.RegularExpressions;
+    public class Program
+    {
+        public static void Main()
+        {
+            var key = int.Parse(Console.ReadLine());
+            var childPattern = @"(?<=@)(?<name>[A-Za-z]+)[^\-|@|:|>|!]+[!][G][!]";
+            var message = Console.ReadLine();
+
+            while (message != "end")
+            {
+                var decryptedMessage = string.Empty;
+
+                foreach (var symbol in message)
+                {
+                    decryptedMessage += ((char)(symbol - key)).ToString();
+                }
+
+                if (Regex.IsMatch(decryptedMessage, childPattern))
+                {
+                    var child = Regex.Match(decryptedMessage, childPattern);
+                    Console.WriteLine(child.Groups["name"].Value);
+                }
+
+                message = Console.ReadLine();
+            }
+        }
+    }
+}
