ae0c0f0080123236a8f4682efcca56bf82283eba
radomirKrastev <radomirkostantinov@gmail.com>
2019-07-28 12:55:38 +0300

    Fundamentals final exam prep

---
+++ Exam - Final Preparation/14 April 2018/Group I/Exam/ArrivingInKathmandu/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ArrivingInKathmandu
+{
+    using System;
+    using System.Text.RegularExpressions;
+    public class Program
+    {
+        public static void Main()
+        {
+            var message = Console.ReadLine();
+            var pattern = @"^(?<peakName>[A-Za-z0-9\!\@\#\$\?]+)=(?<length>[0-9]+)<<(?<geohash>.+)";
+
+            while(message!="Last note")
+            {
+                if (Regex.IsMatch(message, pattern))
+                {
+                    var data = Regex.Match(message, pattern);
+                    var peakName = data.Groups["peakName"].Value;
+                    var length = int.Parse(data.Groups["length"].Value);
+                    var geohashCode = data.Groups["geohash"].Value;
+
+                    if (geohashCode.Length == length)
+                    {
+                        peakName = Regex.Replace(peakName, @"[^\\A-Za-z0-9]", string.Empty);
+                        Console.WriteLine($"Coordinates found! {peakName} -> {geohashCode}");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Nothing found!");
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine("Nothing found!");
+                }
+
+                message = Console.ReadLine();
+            }
+        }
+    }
+}
