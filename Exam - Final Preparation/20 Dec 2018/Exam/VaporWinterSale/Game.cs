d63e0d038c56c729e9e121dc6d55aee1d25ee758
radomirKrastev <radomirkostantinov@gmail.com>
2019-08-01 16:26:08 +0300

    Exam prep problem

---
+++ Exam - Final Preparation/20 Dec 2018/Exam/VaporWinterSale/Game.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VaporWinterSale
+{
+    public class Game
+    {
+        public double Price { get; set; }
+        public string DLC { get; set; }
+    }
+}
---
+++ Exam - Final Preparation/20 Dec 2018/Exam/VaporWinterSale/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace VaporWinterSale
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    public class Program
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries);
+            var games = new Dictionary<string, Game>();
+
+            foreach (var data in input)
+            {
+                var game = new Game();
+                var gameInfo = new List<string>();
+                if (data.Contains('-'))
+                {
+                    gameInfo = data.Split(new string[] { "-" }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                    var name = gameInfo[0];
+                    var price = double.Parse(gameInfo[1]);
+                    game.Price = price;
+                    games.Add(name, game);
+                }
+
+                else
+                {
+                    gameInfo = data.Split(new string[] { ":" }, StringSplitOptions.RemoveEmptyEntries).ToList();
+                    var name = gameInfo[0];
+                    var addition = gameInfo[1];
+                    if (games.ContainsKey(name))
+                    {
+                        games[name].DLC = addition;
+                        games[name].Price += games[name].Price * 0.2;
+                    }
+                }
+            }
+
+            foreach (var game in games)
+            {
+                if (game.Value.DLC == null)
+                {
+                    game.Value.Price -= game.Value.Price * 0.2;
+                }
+
+                else
+                {
+                    game.Value.Price -= game.Value.Price * 0.5;
+                }
+            }
+
+            foreach (var game in games.OrderBy(x=>x.Value.Price))
+            {
+                if (game.Value.DLC != null)
+                {
+                    Console.WriteLine($"{game.Key} - {game.Value.DLC} - {game.Value.Price:F2}");
+                }
+            }
+
+            foreach (var game in games.OrderByDescending(x => x.Value.Price))
+            {
+                if (game.Value.DLC == null)
+                {
+                    Console.WriteLine($"{game.Key} - {game.Value.Price:F2}");
+                }
+            }
+        }
+    }
+}
